bcf5c35c00d8e2f234427f73d9e9d3672cf47b0e
mkusek <mario.kusek@fer.hr>
2017-06-12 10:06:23 +0200

    Just to have previous version in git.

--- src/main/java/hr/fer/tel/scc/service/messaging/RabbitManager.java
+++ src/main/java/hr/fer/tel/scc/service/messaging/RabbitManager.java
@@ -1,13 +1,38 @@
 package hr.fer.tel.scc.service.messaging;
 
+import java.util.LinkedList;
+import java.util.List;
+
 import org.springframework.amqp.rabbit.annotation.Exchange;
 import org.springframework.amqp.rabbit.annotation.Queue;
 import org.springframework.amqp.rabbit.annotation.QueueBinding;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.stereotype.Component;
 
+import hr.fer.tel.scc.service.Book;
+import hr.fer.tel.scc.service.BookService;
+
 @Component
 public class RabbitManager {
+	private BookService service;
+
+	public RabbitManager(BookService service) {
+		this.service = service;
+	}
+
+	@RabbitListener(bindings = @QueueBinding(
+			value = @Queue(),
+			exchange = @Exchange(
+					value = "input",
+					durable="true",
+					autoDelete="false",
+					type="topic"),
+			key = "event"
+	))
+	public void newBook(Book book) {
+		service.newBook(book);
+	}
+
 	
 	@RabbitListener(bindings = @QueueBinding(
 			value = @Queue(),
