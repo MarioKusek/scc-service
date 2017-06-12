3c4f445f262b094122f6abb43f5ef10415f2d234
mkusek <mario.kusek@fer.hr>
2017-06-12 09:55:59 +0200

    This is just to have previous version in git.

--- src/test/java/hr/fer/tel/contracts/SensorBase.java
+++ src/test/java/hr/fer/tel/contracts/SensorBase.java
@@ -2,6 +2,7 @@ package hr.fer.tel.contracts;
 
 import org.apache.commons.collections4.map.HashedMap;
 import org.junit.runner.RunWith;
+import org.springframework.amqp.core.MessageBuilder;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -9,19 +10,36 @@ import org.springframework.cloud.contract.verifier.messaging.MessageVerifier;
 import org.springframework.cloud.contract.verifier.messaging.boot.AutoConfigureMessageVerifier;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import hr.fer.tel.scc.service.Book;
+import hr.fer.tel.scc.service.BookService;
 import hr.fer.tel.scc.service.ServiceApplication;
+import hr.fer.tel.scc.service.messaging.RabbitManager;
 
 @RunWith(SpringRunner.class)
-@SpringBootTest(classes = ServiceApplication.class, webEnvironment = SpringBootTest.WebEnvironment.NONE)
+@SpringBootTest(classes = {ServiceApplication.class, RabbitManager.class}, webEnvironment = SpringBootTest.WebEnvironment.NONE)
+
 @AutoConfigureMessageVerifier
 public abstract class SensorBase {
-	@Autowired
-	private MessageVerifier verifier;
+	@Autowired private MessageVerifier verifier;
 	
 	@Autowired RabbitTemplate rabbitTemplate;
+	@Autowired BookService bookService;
+	
+	public void bookReturnedTriggered() {
+		bookService.sendBook(new Book("foo"));
+	}
+	
+	public void bookEventWasReceived() {
+		// TODO check that book was received
+	}
 	
 	public void emitGetSensorDataEvent() {
-		verifier.send("", new HashedMap<>(), "sensor-service");
 		//rabbitTemplate.convertSendAndReceive("sensor-service", "sensor.read", "");
+//		rabbitTemplate.send("sensor-service", "sensor.read",
+//              MessageBuilder.withBody("{ \"value\": 45, \"name\": \"testSensor\" }".getBytes()).build());
+		rabbitTemplate.send("sensor-service", "sensor.read", 
+				MessageBuilder.withBody("".getBytes())
+					.setReplyTo("sensor-client")
+					.build());
 	}
 }
